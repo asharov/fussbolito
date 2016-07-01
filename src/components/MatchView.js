90dcd18a29d4b28f55c714edde30c501b6d6bce6
Jaakko Kangasharju <jkan@futurice.com>
2016-07-01 16:46:57 +0200

    Remove default screen

---
+++ src/components/MatchView.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+  View
+} from 'react-native'
+
+const MatchView = React.createClass({
+  render() {
+    return (
+      <View style={{flex: 1, backgroundColor: 'red'}}/>
+    )
+  }
+})
+
+export default MatchView
